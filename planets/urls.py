0e7cb5e41ff0ece53a6a3ecfaaaa273725ea0ec2
Jan Chwiejczak <jchwiejczak@gmail.com>
2018-09-17 14:19:41 +0100

    Add new endpoint for single planet

--- planets/urls.py
+++ planets/urls.py
@@ -5,6 +5,7 @@ from planets import views
 
 urlpatterns = [
     url(r'^planets', views.PlanetList.as_view()),
+    url(r'^planet/(?P<name>[a-zA-Z]+)', views.SinglePlanet.as_view())
 ]
 
 urlpatterns = format_suffix_patterns(urlpatterns)
--- planets/views.py
+++ planets/views.py
@@ -6,3 +6,8 @@ from planets.models import Planet
 class PlanetList(generics.ListCreateAPIView):
     queryset = Planet.objects.all()
     serializer_class = PlanetSerializer
+
+class SinglePlanet(generics.RetrieveDestroyAPIView):
+    lookup_field = 'name'
+    serializer_class = PlanetSerializer
+    queryset = Planet.objects.all()
