627574b478b84825db13d865fe24109f0b17e6c7
Jan Chwiejczak <jchwiejczak@gmail.com>
2018-09-17 12:43:15 +0100

    Add Model for Planet migrate Django DB

--- planets/models.py
+++ planets/models.py
@@ -1,1 +1,7 @@
 from django.db import models
+
+class Planet(models.Model):
+    name = models.CharField(max_length=255, primary_key=True)
+    diameter = models.FloatField()
+    distance_from_sun = models.FloatField()
+    number_of_moons = models.IntegerField()
